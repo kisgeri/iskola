cf34f0a2de3b7e184f4e0bb57506cc8299adf3e0
Keniser <102257694+kisgeri@users.noreply.github.com>
2025-02-14 12:03:09 +0100

    Add files via upload

---
+++ weiss/projekt/11.osztaly/casino_project/slotmachine/slotmachine/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace slotmachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(217, 195);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(291, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(571, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(217, 195);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Goldenrod;
+            this.pictureBox3.Location = new System.Drawing.Point(1, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(798, 230);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F);
+            this.button1.Location = new System.Drawing.Point(604, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 99);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SPIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(408, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(799, 445);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ weiss/projekt/11.osztaly/casino_project/slotmachine/slotmachine/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slotmachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("1.png");
+            pictureBox2.Image = Image.FromFile("2.png");
+            pictureBox6.Image = Image.FromFile("3.png");
+
+        }
+
+        Random rnd = new Random();
+
+
+        int szam1, szam2, szam3, move;
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            button1.Visible = false;
+            label1.Visible = false;
+
+        }//https://stackoverflow.com/questions/77984684/im-trying-to-have-a-picture-box-switch-between-4-images-to-simulate-a-spinning
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move++;
+            if(move<30)
+            {
+                szam1 = rnd.Next(1, 7);
+                szam2 = rnd.Next(1, 7);
+                szam3 = rnd.Next(1, 7);
+
+                pictureBox1.Image = Image.FromFile(szam1.ToString() + ".png");
+                pictureBox2.Image = Image.FromFile(szam2.ToString() + ".png");
+                pictureBox6.Image = Image.FromFile(szam3.ToString() + ".png");
+            }
+            else
+            {
+                timer1.Enabled = false;
+                move = 0;
+                label1.Visible = true;
+                if(szam1 == 1 && szam2 == 1 && szam3 == 1)
+                {
+                    label1.Text = "szilva";
+                }
+                else if (szam1 == 2 && szam2 == 2 && szam3 == 2)
+                {
+                    label1.Text = "narancs";
+                }
+                else if (szam1 == 3 && szam2 == 3 && szam3 == 3)
+                {
+                    label1.Text = "cseresznye";
+                }
+                else if (szam1 == 4 && szam2 == 4 && szam3 == 4)
+                {
+                    label1.Text = "csengő";
+                }
+                else if (szam1 == 5 && szam2 == 5 && szam3 == 5)
+                {
+                    label1.Text = "bar";
+                }
+                else if (szam1 == 6 && szam2 == 6 && szam3 == 6)
+                {
+                    label1.Text = "jackpot";
+                }
+                else
+                { label1.Text = "nem nyertél :("; }
+
+                button1.Visible = true;
+            }
+
+        }
+        //https://www.youtube.com/watch?v=E9t3l10swkw&t=1149s
+    }
+}
