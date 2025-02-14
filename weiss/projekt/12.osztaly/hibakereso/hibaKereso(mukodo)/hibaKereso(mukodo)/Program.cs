b7da9bd90bfaf83bf36de07dfd9cdec804193d43
Keniser <102257694+kisgeri@users.noreply.github.com>
2025-02-14 11:48:22 +0100

    Add files via upload

---
+++ weiss/projekt/12.osztaly/hibakereso/hibaKereso(mukodo)/hibaKereso(mukodo)/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace hibaKereso_mukodo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kiiras();
+
+        }
+
+        private static void kiiras()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Feladatsor:");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nÁTLAG:");
+            atlag();
+
+            Console.WriteLine("\nELDÖNTÉS:");
+            if (kereses())
+            {
+                Console.WriteLine("van a szövegben space");
+            }
+            else
+            {
+                Console.WriteLine("nincs a szövegbe space");
+            }
+
+            Console.WriteLine("\nKIVÁLASZTÁS:");
+            int[] szamok;
+            szamok = new int[] { -11, -7, 5, -1, 2, 5, 8, 11, 14, 17, -11, -7, 5, -1 };
+            kivalasztas(szamok);
+        }
+
+        private static void kivalasztas(int[] szamok)
+        {
+            int i = 0;
+            bool parosszam = false;
+            int elsoparosszam=0;
+            while (i < szamok.Length && parosszam==false)
+            {
+                if (szamok[i] % 2 == 0)
+                {
+                    elsoparosszam = szamok[i];
+                    parosszam = true;
+                    
+                }
+                i++;
+            }
+
+            Console.WriteLine("AZ első páros szám a "+elsoparosszam);
+            
+        }
+
+        private static bool kereses()
+        {
+           
+            string szoveg;
+            char jel = ' ';
+
+            Console.Write("írj be egy szöveget: ");
+            szoveg = Console.ReadLine();
+            Console.WriteLine();
+
+            for(int i=0; i<szoveg.Length;i++)
+            {
+
+
+                if (szoveg[i] == jel)
+                {
+                    return true;
+                }
+
+
+            }
+            return false;
+            
+        }
+
+        private static void atlag()
+        {
+
+            int darab;
+            double atlag;
+
+            Console.Write("hány darab számnak akarod tudni az átlagát: ");
+            darab = Convert.ToInt32(Console.ReadLine());
+
+            atlag = 0;
+
+            for (int i = 0; i < darab; i++)
+            {
+
+                Console.Write("ad meg a jegyeidet: ");
+                atlag += Convert.ToDouble(Console.ReadLine());
+
+            }
+            Console.WriteLine();
+            atlag = (atlag / darab);
+            atlag = Math.Round(atlag, 2);
+            Console.WriteLine("jegyeid összátlaga: {0}",  atlag);
+        }
+    }
+}
---
+++ weiss/projekt/12.osztaly/otoslotto/otoslotto/otoslotto/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otoslotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ötöslottó program\n");
+
+            Random rnd = new Random();
+            int[] nyeroszamok = new int[5];
+            int talalatok = 0;
+            int[] tip = new int[5];
+
+            Console.WriteLine("kérem adja meg a tippjeit:");
+
+            nyeroszamok[0] = rnd.Next(1, 91);
+
+            Console.Write(1 + ". szám tipp: ");
+            tip[0] = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<nyeroszamok.Length;i++)
+            {
+
+                do
+                {
+                    nyeroszamok[i] = rnd.Next(1, 91);
+                } while (nyeroszamok[i-1] == nyeroszamok[i]);
+                
+            }
+
+            for (int i = 1; i<tip.Length;i++)
+            {
+                do
+                {
+                    do
+                    {
+                        Console.Write((i + 1) + ". szám tipp: ");
+                        tip[i] = int.Parse(Console.ReadLine());
+
+                        if (tip[i] < 1 || tip[i] > 90)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("A megadott szám nem 1 és 90 közöt, kérlek adj meg másikat");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                    } while (tip[i] < 1 || tip[i] > 90);
+
+                    if(tip[i - 1] == tip[i])
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("A megadott szám egyezik egy korábival, kérlek adj meg másikat");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                } while (tip[i - 1] == tip[i]);
+      
+            }
+
+            for (int i = 0; i< nyeroszamok.Length;i++)
+            {
+                
+                for (int j = 0; j < tip.Length; j++)
+                {
+                    if (tip[j] == nyeroszamok[i])
+                    {
+                        talalatok++;
+                    }
+                }
+            }
+
+            ShellRendezes(nyeroszamok);
+
+            Console.Write("\nA nyerő számok: ");
+            for(int i = 0; i<nyeroszamok.Length;i++)
+            {
+                Console.Write(nyeroszamok[i]+"; ");
+            }
+            Console.WriteLine("\nA találataid száma: "+talalatok);
+
+            Console.ReadKey();
+        }
+
+        private static void ShellRendezes(int[] a)
+        {
+            Int32 gap, n, i, j, x, y;
+
+            gap = 1;
+            n = a.Length;
+
+            while (gap * 2 <= n)
+            {
+                gap = gap * 2;
+            }
+
+            gap = gap - 1;
+
+            do
+            {
+                i = 0;
+
+                while ((i <= gap) && (i + gap < n))
+                {
+                    j = i + gap;
+
+                    while (j < n)
+                    {
+                        x = a[j];
+                        y = j - gap;
+
+                        while ((y > -1) && (x < a[y]))
+                        {
+                            a[y + gap] = a[y];
+                            y = y - gap;
+                        }
+                        a[y + gap] = x;
+                        j = j + gap;
+                    }
+                    i = i + 1;
+                }
+                gap = gap / 2;
+
+            } while (gap > 0);
+
+        }
+    }
+}
