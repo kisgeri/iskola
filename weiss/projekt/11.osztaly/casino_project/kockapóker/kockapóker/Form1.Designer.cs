348d17d77dc750cd529dbf84b46f76a1a22faa69
Keniser <102257694+kisgeri@users.noreply.github.com>
2025-02-14 12:04:13 +0100

    Add files via upload

---
+++ weiss/projekt/11.osztaly/casino_project/kockapóker/kockapóker/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace kockapóker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 195);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "press";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(108, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(224, 85);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(338, 85);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(83, 85);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(459, 85);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(83, 85);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(576, 85);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(83, 85);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox1.Location = new System.Drawing.Point(67, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 24);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(17, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "bet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "credit:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(66, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(269, 393);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "not int/empty";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label6.Location = new System.Drawing.Point(335, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ weiss/projekt/11.osztaly/casino_project/kockapóker/kockapóker/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace kockapóker
+{
+    
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        /*int szam1, szam2, szam3, szam4, szam5;*/
+        int bet,credit;
+        public Form1()
+        {
+            credit = 1000;
+            
+            InitializeComponent();
+            label4.Text = Convert.ToString(credit);
+            label5.Visible = true;
+            label6.Visible = false;
+        }
+        
+
+
+
+            private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text != "0")
+            {
+                if (int.TryParse(textBox1.Text, out bet))
+                {
+                    bet = int.Parse(textBox1.Text);
+                    if (bet < 1)
+                    {
+                        label5.Visible = true;
+                    }
+                    else
+                    {
+                        label5.Visible = false;
+                    }
+                }
+                else { label5.Visible = true; }
+
+            }
+            else { label5.Visible = true; }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text != "" && bet >= 1)
+            {
+                if(credit >= bet)
+                {
+                    credit = credit - bet;
+                    label4.Text = Convert.ToString(credit);
+                    label6.Visible = true;
+                    /*
+                    szam1 = rnd.Next(1, 6);
+                    szam2 = rnd.Next(1, 6);
+                    szam3 = rnd.Next(1, 6);
+                    szam4 = rnd.Next(1, 6);
+                    szam5 = rnd.Next(1, 6);
+                    */
+                    int[] tömb = new int[6];
+                    bool kissor = true;
+                    bool nagysor = true;
+                    Dictionary<int, int> mergedParts = new Dictionary<int, int>();
+                    for (int i = 1; i < tömb.Length; i++)
+                    {
+                        tömb[i] = rnd.Next(1, 7);
+                        if (i == 1) { pictureBox1.Image = Image.FromFile(tömb[i].ToString() + ".png"); }
+                        if (i == 2)
+                        {
+                            pictureBox2.Image = Image.FromFile(tömb[i].ToString() + ".png");
+                        }
+                        if (i == 3)
+                        {
+                            pictureBox3.Image = Image.FromFile(tömb[i].ToString() + ".png");
+                        }
+                        if (i == 4)
+                        {
+                            pictureBox4.Image = Image.FromFile(tömb[i].ToString() + ".png");
+                        }
+                        if (i == 5)
+                        {
+                            pictureBox5.Image = Image.FromFile(tömb[i].ToString() + ".png");
+                        }
+                        if (tömb[i] != i + 1)
+                        {
+                            kissor = false;
+                        }
+                        if (tömb[i] != i + 2)
+                        {
+                            nagysor = false;
+                        }
+                    }
+               
+                    
+                    
+                    for (int i = 0; i < tömb.Length; i++)
+                    {
+                        if (mergedParts.ContainsKey(tömb[i]))
+                        {
+                            mergedParts[tömb[i]] += 1;
+                        }
+                        else
+                        {
+                            mergedParts[tömb[i]] = 1;
+                        }
+                    }
+
+                    int count = 0;
+                    bool harmas_par = false;
+                    bool par = false;
+                    bool poker = false;
+                    bool royalpoker = false;
+                    bool full = false;
+                    foreach (var item in mergedParts)
+                    {
+                        if (item.Value == 2)
+                        {
+                            
+                            count += 1;
+                            if (count <= 2)
+                            {
+                                
+                                label6.Text = count + " pár";
+                                par = true;
+                            }
+
+
+
+
+                        }
+                        
+                        
+
+
+                        if (item.Value == 3)
+                        {
+                            label6.Text = "DRILL";
+                            harmas_par = true;
+                            
+                        }
+                        if (par && harmas_par)
+                        {
+                            par = false;
+                            harmas_par = false;
+                            full = true;
+                            label6.Text = "FULL";
+                            bet = bet + 300;
+                            credit = credit + bet;
+                            label4.Text = Convert.ToString(credit);
+                        }
+                        if (item.Value == 4)
+                        {
+                            poker = true;
+                            label6.Text = "POKER";
+                            bet = bet + 400;
+                            credit = credit + bet;
+                            label4.Text = Convert.ToString(credit);
+                            
+                        }
+                        if (item.Value == 5)
+                        {
+                            royalpoker = true;
+                            label6.Text = "ROYAL POKER";
+                            bet = bet + 500;
+                            credit = credit + bet;
+                            label4.Text = Convert.ToString(credit);
+
+                        }
+                        if (par == false && harmas_par==false&&poker==false&&royalpoker==false&& full == false)
+                        {
+                            label6.Text = "nem nyertél";
+
+                        }
+                        
+                    }
+                    if (kissor)
+                    {
+                        label6.Text = "Kissor";
+                        bet = bet + 700;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+                    else if (nagysor)
+                    {
+                        label6.Text = "Nagysor";
+                        bet = bet + 700;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+
+                    
+
+                    if (harmas_par==true)
+                    {
+                        bet = bet + 150;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+
+                    if (par == true && count >= 2)
+                    {
+                        label6.Text = "dupla pár";
+                        bet = bet + 100;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+                    else if(par == true)
+                    {
+
+                        bet = bet + 50;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+
+
+                    /*
+                    if (par==true)
+                    {
+                        bet = bet + 50;
+                        credit = credit + bet;
+                        label4.Text = Convert.ToString(credit);
+                    }
+                    */
+                    /*
+                    pictureBox1.Image = Image.FromFile(szam1.ToString() + ".png");
+                    pictureBox2.Image = Image.FromFile(szam2.ToString() + ".png");
+                    pictureBox3.Image = Image.FromFile(szam3.ToString() + ".png");
+                    pictureBox4.Image = Image.FromFile(szam4.ToString() + ".png");
+                    pictureBox5.Image = Image.FromFile(szam5.ToString() + ".png");
+                    */
+
+                }
+
+
+
+                bet = 0;
+                textBox1.Text = "";
+            }
+            else { label5.Visible = true; }
+            
+
+        }
+        
+    }
+}
