1d746ffbc9ac0e74459fa00f10f9716d63aa6177
kisgeri <102257694+kisgeri@users.noreply.github.com>
2025-11-02 22:54:01 +0100

    Add files via upload

---
+++ weiss/programozas/C#/13.osztaly/UltraBalatonHibas_KM/ultraBalaton/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ultraBalaton
+{
+    class Program
+    {
+
+        struct VersenyzoStruktura
+        {
+
+            public string versenyzoNev;
+            public int rajtszam;
+            public string kategoria;
+            public double versenyidoO;
+            public double versenyidoP;
+            public double versenyidoS;
+            public int tavszazalek;
+        }
+
+        static List<VersenyzoStruktura> adatok = new List<VersenyzoStruktura>();
+
+        static void Main(string[] args)
+        {
+
+            feladat2();
+
+            feladat3();
+
+            feladat4();
+
+            feladat5();
+
+            feladat7();
+
+            feladat8();
+
+            Console.ReadKey();
+
+
+        }
+
+        private static void feladat8()
+        {
+            double NoiLegjobbido = double.MaxValue;
+            double aktualisido;
+            double oraNoiLegjobb = 0;
+            double percNoiLegjobb = 0;
+            double mspercNoiLegjobb = 0;
+            string nev = "";
+            int versenyzoSzama = 0;
+
+
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].kategoria == "Noi" && adatok[i].tavszazalek == 100)
+                {
+                    aktualisido = IdoOraban(adatok[i].versenyidoO, adatok[i].versenyidoP, adatok[i].versenyidoS);
+                    if (aktualisido < NoiLegjobbido)
+                    {
+                        NoiLegjobbido = aktualisido; 
+                        oraNoiLegjobb = adatok[i].versenyidoO;
+                        percNoiLegjobb = adatok[i].versenyidoP;
+                        mspercNoiLegjobb = adatok[i].versenyidoS;
+                        nev = adatok[i].versenyzoNev;
+                        versenyzoSzama = adatok[i].rajtszam;
+                    }
+                }
+            }
+
+            Console.WriteLine("8. feladat: Verseny győztesei");
+            Console.WriteLine("Nők: {0} ({1}.) - {2}:{3}:{4}", nev, versenyzoSzama, oraNoiLegjobb, percNoiLegjobb, mspercNoiLegjobb);
+
+            int oraFerfi = 0;
+        }
+
+        private static void feladat7()
+        {
+            int ora;
+            int perc;
+            int msperc;
+            double idoOrabanOssz = 0;
+            int osszFerfi = 0;
+
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].kategoria == "Ferfi" && adatok[i].tavszazalek == 100)
+                {
+
+
+                    idoOrabanOssz = idoOrabanOssz + IdoOraban(adatok[i].versenyidoO, adatok[i].versenyidoP, adatok[i].versenyidoS);
+                   
+                    osszFerfi++;
+                }
+            }
+
+            Console.WriteLine("7. feladat: Átlagos idő: {0} óra", idoOrabanOssz / osszFerfi);
+
+
+
+
+
+        }
+
+        private static double IdoOraban(double ora, double perc, double msperc)
+        {
+
+
+            double teljesIdoOraban = ora + (perc / 60) + (msperc / 3600);
+
+            return teljesIdoOraban;
+        }
+
+        private static void feladat5()
+        {
+            Console.Write("5. feladat: Kérem a sportoló nevét: ");
+            string versenyzoNev = Console.ReadLine();
+
+            int szamlalo = 0;
+            string versenyzoVolt = "Nem";
+            string versenyzoVegzett = "Nem";
+            double ora = 0;
+            double perc = 0;
+            double msperc = 0;
+
+
+            while (szamlalo < adatok.Count && versenyzoVolt != "Igen")
+            {
+
+                if (adatok[szamlalo].versenyzoNev == versenyzoNev)
+                {
+                    versenyzoVolt = "Igen";
+
+                    if (adatok[szamlalo].tavszazalek == 100)
+                    {
+                        versenyzoVegzett = "Igen";
+                    }
+                    ora = adatok[szamlalo].versenyidoO;
+                    perc = adatok[szamlalo].versenyidoP;
+                    msperc = adatok[szamlalo].versenyidoS;
+                }
+
+                szamlalo++;
+
+            }
+
+            double teljesIdoOraban = IdoOraban(ora, perc, msperc);
+
+            Console.WriteLine("\tIndult egyéniben a sportoló? {0}", versenyzoVolt);
+            if (versenyzoVolt == "Igen")
+            {
+                Console.WriteLine("\tTeljesítette a teljes távot? {0}", versenyzoVegzett);
+            }
+        }
+
+        private static void feladat4()
+        {
+            Console.Write("4. feladat: Célba érkező női sportolók: ");
+
+            int noiTeljesTavDb = 0;
+
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].kategoria == "Noi" && adatok[i].tavszazalek == 100)
+                {
+                    noiTeljesTavDb++;
+                }
+            }
+
+            Console.WriteLine("{0} fő.", noiTeljesTavDb);
+
+        }
+
+        private static void feladat3()
+        {
+
+            Console.WriteLine("3. Feladat: Egyéni indulók: {0} fő", adatok.Count);
+        }
+
+        private static void feladat2()
+        {
+
+            StreamReader olvasocsatorna = new StreamReader(@"D:\Ultrabalaton\Ultrabalaton\Ultrabalaton\Ultrabalaton\ub2017egyeni.txt", true);
+
+            string elsosor = olvasocsatorna.ReadLine();
+            string sor;
+
+
+
+            string[] darabol;
+
+            while (!olvasocsatorna.EndOfStream)
+            {
+                sor = olvasocsatorna.ReadLine();
+                darabol = sor.Split(';');
+                VersenyzoStruktura eredmeny = new VersenyzoStruktura();
+
+                eredmeny.versenyzoNev = darabol[0];
+                eredmeny.rajtszam = Convert.ToInt32(darabol[1]);
+                eredmeny.kategoria = darabol[2];
+
+                string[] ido = darabol[3].Split(':');
+
+                eredmeny.versenyidoO = Convert.ToDouble(ido[0]);
+                eredmeny.versenyidoP = Convert.ToDouble(ido[1]);
+                eredmeny.versenyidoS = Convert.ToDouble(ido[2]);
+                eredmeny.tavszazalek = Convert.ToInt32(darabol[4]);
+
+                adatok.Add(eredmeny);
+            }
+
+            olvasocsatorna.Close();
+
+        }
+    }
+}
---
+++ weiss/programozas/C#/13.osztaly/Ultrabalaton/Ultrabalaton/Ultrabalaton/Ultrabalaton/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ultrabalaton
+{
+    /*
+     39
+         */
+    class Program
+    {
+        //2. Feladat struktúra fájl tárolás
+        struct BalatonStruktura
+        {
+            public string Versenyzo;
+            public int Rajtszam;
+            public string Kategoria;
+            public string Versenyido;
+            public int TavSzazalek;
+        }
+        static void Main(string[] args)
+        {
+            feladatok();
+            Console.ReadKey();
+        }
+
+        private static void feladatok()
+        {
+            StreamReader olvasocsatorna = new StreamReader(@"D:\Ultrabalaton\Ultrabalaton\Ultrabalaton\Ultrabalaton\ub2017egyeni.txt");
+
+            string elsosor = olvasocsatorna.ReadLine();
+            string sorok;
+
+            List<BalatonStruktura> versenyzok = new List<BalatonStruktura>();
+            string[] darabol;
+
+            while (!olvasocsatorna.EndOfStream)
+            {
+                sorok = olvasocsatorna.ReadLine();
+                darabol = sorok.Split(';');
+
+                BalatonStruktura balaton = new BalatonStruktura();
+
+                balaton.Versenyzo = darabol[0];
+                balaton.Rajtszam = int.Parse(darabol[1]);
+                balaton.Kategoria = darabol[2];
+                balaton.Versenyido = darabol[3];
+                balaton.TavSzazalek = int.Parse(darabol[4]);
+
+                versenyzok.Add(balaton);
+            }
+
+            
+            Console.WriteLine("3. Feladat: Egyéni indulók: {0} fő", versenyzok.Count());
+
+            
+            int teljesito_nok_szama = 0;
+            for (int i = 0; i < versenyzok.Count(); i++)
+            {
+                if (versenyzok[i].Kategoria.ToLower() == "noi" && versenyzok[i].TavSzazalek == 100)
+                {
+                    teljesito_nok_szama++;
+                }
+            }
+            Console.WriteLine("\n4. Feladat: Célba érkező női sportolók: {0} fő", teljesito_nok_szama);
+
+            
+            Console.Write("\n5. Feladat: Kérem a sportoló nevét: ");
+            string versenyzo_nev = Console.ReadLine();
+            bool indult = false;
+            bool teljesitete = false;
+            //while legyen
+            for (int i = 0; i < versenyzok.Count(); i++)
+            {
+                if (versenyzo_nev.ToLower() == versenyzok[i].Versenyzo.ToLower())
+                {
+                    indult = true;
+                }
+                if (versenyzo_nev == versenyzok[i].Versenyzo && versenyzok[i].TavSzazalek == 100)
+                {
+                    teljesitete = true;
+                }
+            }
+
+            if (indult == true)
+            {
+                Console.WriteLine("\tIndult egyéniben a sportoló? Igen");
+                if (teljesitete == true)
+                {
+                    Console.WriteLine("\tTeljesítette a teljes távot? Igen");
+                }
+                else
+                {
+                    Console.WriteLine("\tTeljesítette a teljes távot? Nem");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tIndult egyéniben a sportoló? Nem");
+            }
+
+            
+            double oszes_ido = 0;
+            int ferfiak_szama = 0;
+
+            for (int i = 0; i < versenyzok.Count(); i++)
+            {
+                if (versenyzok[i].Kategoria.ToLower() == "ferfi" && versenyzok[i].TavSzazalek == 100)
+                {
+                    oszes_ido += IdoOraban(versenyzok[i].Versenyido);
+                    ferfiak_szama += 1;
+                }
+            }
+            Console.WriteLine("\n7. Feladat: Átlagos idő: {0} óra", oszes_ido / ferfiak_szama);
+
+            
+            Console.WriteLine("\n8. Feladat: Verseny győztesei");
+
+            double noi_legjobb_ido = double.MaxValue;
+            double ferfi_legjobb_ido = double.MaxValue;
+            double aktualis_ido;
+
+            string noi_gyoztes_adatok = "";
+            string ferfi_gyoztes_adatok = "";
+
+            for (int i = 0; i < versenyzok.Count(); i++)
+            {
+                if (versenyzok[i].TavSzazalek == 100)
+                {
+                    aktualis_ido = IdoOraban(versenyzok[i].Versenyido);
+                    if (versenyzok[i].Kategoria.ToLower() == "noi" && aktualis_ido < noi_legjobb_ido)
+                    {
+                        noi_legjobb_ido = aktualis_ido;
+                        noi_gyoztes_adatok = $"\tNők: {versenyzok[i].Versenyzo} ({versenyzok[i].Rajtszam}.) - {versenyzok[i].Versenyido}";
+                    }
+                    if (versenyzok[i].Kategoria.ToLower() == "ferfi" && aktualis_ido < ferfi_legjobb_ido)
+                    {
+                        ferfi_legjobb_ido = aktualis_ido;
+                        ferfi_gyoztes_adatok = $"\n\tFérfiak: {versenyzok[i].Versenyzo} ({versenyzok[i].Rajtszam}.) - {versenyzok[i].Versenyido}";
+                    }
+                }
+            }
+
+            Console.WriteLine(noi_gyoztes_adatok+ ferfi_gyoztes_adatok);
+        }
+
+        // 6. Feladathoz
+        static double IdoOraban(string verseny_ido)
+        {
+            string[] ido_darabol = verseny_ido.Split(':');
+            double ora = double.Parse(ido_darabol[0]);
+            double perc = double.Parse(ido_darabol[1]);
+            double masodperc = double.Parse(ido_darabol[2]);
+
+            return ora + (perc / 60.0) + (masodperc / 3600.0);
+        }
+    }
+}
