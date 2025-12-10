128c2186f6d99e53dde8c2ae65c2daa2a25f9cc3
kisgeri <102257694+kisgeri@users.noreply.github.com>
2025-12-10 13:48:39 +0100

    Add files via upload

---
+++ weiss/programozas/C#/13.osztaly/betuStilus/betuStilus/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace betuStilus
+{
+    partial class betustilus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cimkelbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.athuzott = new System.Windows.Forms.CheckBox();
+            this.alahuzott = new System.Windows.Forms.CheckBox();
+            this.dolt = new System.Windows.Forms.CheckBox();
+            this.felkover = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.betumeret18 = new System.Windows.Forms.RadioButton();
+            this.betumeret16 = new System.Windows.Forms.RadioButton();
+            this.betumeret14 = new System.Windows.Forms.RadioButton();
+            this.betumeret12 = new System.Windows.Forms.RadioButton();
+            this.betumeret11 = new System.Windows.Forms.RadioButton();
+            this.betumeret10 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fekete = new System.Windows.Forms.RadioButton();
+            this.kek = new System.Windows.Forms.RadioButton();
+            this.piros = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cimkelbl
+            // 
+            this.cimkelbl.Location = new System.Drawing.Point(72, 33);
+            this.cimkelbl.Name = "cimkelbl";
+            this.cimkelbl.Size = new System.Drawing.Size(613, 53);
+            this.cimkelbl.TabIndex = 0;
+            this.cimkelbl.Text = "Árvíztűrő tükörfúrógép";
+            this.cimkelbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.athuzott);
+            this.groupBox1.Controls.Add(this.alahuzott);
+            this.groupBox1.Controls.Add(this.dolt);
+            this.groupBox1.Controls.Add(this.felkover);
+            this.groupBox1.Location = new System.Drawing.Point(24, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 231);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Betűstílus";
+            // 
+            // athuzott
+            // 
+            this.athuzott.AutoSize = true;
+            this.athuzott.Location = new System.Drawing.Point(6, 138);
+            this.athuzott.Name = "athuzott";
+            this.athuzott.Size = new System.Drawing.Size(65, 17);
+            this.athuzott.TabIndex = 7;
+            this.athuzott.Text = "Áthúzott";
+            this.athuzott.UseVisualStyleBackColor = true;
+            this.athuzott.CheckedChanged += new System.EventHandler(this.athuzott_CheckedChanged);
+            // 
+            // alahuzott
+            // 
+            this.alahuzott.AutoSize = true;
+            this.alahuzott.Location = new System.Drawing.Point(6, 103);
+            this.alahuzott.Name = "alahuzott";
+            this.alahuzott.Size = new System.Drawing.Size(70, 17);
+            this.alahuzott.TabIndex = 6;
+            this.alahuzott.Text = "Aláhúzott";
+            this.alahuzott.UseVisualStyleBackColor = true;
+            this.alahuzott.CheckedChanged += new System.EventHandler(this.alahuzott_CheckedChanged);
+            // 
+            // dolt
+            // 
+            this.dolt.AutoSize = true;
+            this.dolt.Location = new System.Drawing.Point(6, 63);
+            this.dolt.Name = "dolt";
+            this.dolt.Size = new System.Drawing.Size(45, 17);
+            this.dolt.TabIndex = 5;
+            this.dolt.Text = "Dölt";
+            this.dolt.UseVisualStyleBackColor = true;
+            this.dolt.CheckedChanged += new System.EventHandler(this.dolt_CheckedChanged);
+            // 
+            // felkover
+            // 
+            this.felkover.AutoSize = true;
+            this.felkover.Location = new System.Drawing.Point(6, 30);
+            this.felkover.Name = "felkover";
+            this.felkover.Size = new System.Drawing.Size(67, 17);
+            this.felkover.TabIndex = 4;
+            this.felkover.Text = "Félkövér";
+            this.felkover.UseVisualStyleBackColor = true;
+            this.felkover.CheckedChanged += new System.EventHandler(this.felkover_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.betumeret18);
+            this.groupBox2.Controls.Add(this.betumeret16);
+            this.groupBox2.Controls.Add(this.betumeret14);
+            this.groupBox2.Controls.Add(this.betumeret12);
+            this.groupBox2.Controls.Add(this.betumeret11);
+            this.groupBox2.Controls.Add(this.betumeret10);
+            this.groupBox2.Location = new System.Drawing.Point(257, 90);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(192, 231);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Betűméret";
+            // 
+            // betumeret18
+            // 
+            this.betumeret18.AutoSize = true;
+            this.betumeret18.Location = new System.Drawing.Point(97, 103);
+            this.betumeret18.Name = "betumeret18";
+            this.betumeret18.Size = new System.Drawing.Size(37, 17);
+            this.betumeret18.TabIndex = 9;
+            this.betumeret18.TabStop = true;
+            this.betumeret18.Text = "18";
+            this.betumeret18.UseVisualStyleBackColor = true;
+            this.betumeret18.CheckedChanged += new System.EventHandler(this.betumeret18_CheckedChanged);
+            // 
+            // betumeret16
+            // 
+            this.betumeret16.AutoSize = true;
+            this.betumeret16.Location = new System.Drawing.Point(97, 63);
+            this.betumeret16.Name = "betumeret16";
+            this.betumeret16.Size = new System.Drawing.Size(37, 17);
+            this.betumeret16.TabIndex = 8;
+            this.betumeret16.TabStop = true;
+            this.betumeret16.Text = "16";
+            this.betumeret16.UseVisualStyleBackColor = true;
+            this.betumeret16.CheckedChanged += new System.EventHandler(this.betumeret16_CheckedChanged);
+            // 
+            // betumeret14
+            // 
+            this.betumeret14.AutoSize = true;
+            this.betumeret14.Location = new System.Drawing.Point(97, 30);
+            this.betumeret14.Name = "betumeret14";
+            this.betumeret14.Size = new System.Drawing.Size(37, 17);
+            this.betumeret14.TabIndex = 7;
+            this.betumeret14.TabStop = true;
+            this.betumeret14.Text = "14";
+            this.betumeret14.UseVisualStyleBackColor = true;
+            this.betumeret14.CheckedChanged += new System.EventHandler(this.betumeret14_CheckedChanged);
+            // 
+            // betumeret12
+            // 
+            this.betumeret12.AutoSize = true;
+            this.betumeret12.Location = new System.Drawing.Point(6, 103);
+            this.betumeret12.Name = "betumeret12";
+            this.betumeret12.Size = new System.Drawing.Size(37, 17);
+            this.betumeret12.TabIndex = 6;
+            this.betumeret12.TabStop = true;
+            this.betumeret12.Text = "12";
+            this.betumeret12.UseVisualStyleBackColor = true;
+            this.betumeret12.CheckedChanged += new System.EventHandler(this.betumeret12_CheckedChanged);
+            // 
+            // betumeret11
+            // 
+            this.betumeret11.AutoSize = true;
+            this.betumeret11.Location = new System.Drawing.Point(6, 62);
+            this.betumeret11.Name = "betumeret11";
+            this.betumeret11.Size = new System.Drawing.Size(37, 17);
+            this.betumeret11.TabIndex = 5;
+            this.betumeret11.TabStop = true;
+            this.betumeret11.Text = "11";
+            this.betumeret11.UseVisualStyleBackColor = true;
+            this.betumeret11.CheckedChanged += new System.EventHandler(this.betumeret11_CheckedChanged);
+            // 
+            // betumeret10
+            // 
+            this.betumeret10.AutoSize = true;
+            this.betumeret10.Location = new System.Drawing.Point(6, 29);
+            this.betumeret10.Name = "betumeret10";
+            this.betumeret10.Size = new System.Drawing.Size(37, 17);
+            this.betumeret10.TabIndex = 4;
+            this.betumeret10.TabStop = true;
+            this.betumeret10.Text = "10";
+            this.betumeret10.UseVisualStyleBackColor = true;
+            this.betumeret10.CheckedChanged += new System.EventHandler(this.betumeret10_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fekete);
+            this.panel1.Controls.Add(this.kek);
+            this.panel1.Controls.Add(this.piros);
+            this.panel1.Location = new System.Drawing.Point(535, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 231);
+            this.panel1.TabIndex = 3;
+            // 
+            // fekete
+            // 
+            this.fekete.AutoSize = true;
+            this.fekete.Location = new System.Drawing.Point(46, 102);
+            this.fekete.Name = "fekete";
+            this.fekete.Size = new System.Drawing.Size(58, 17);
+            this.fekete.TabIndex = 12;
+            this.fekete.TabStop = true;
+            this.fekete.Text = "Fekete";
+            this.fekete.UseVisualStyleBackColor = true;
+            this.fekete.CheckedChanged += new System.EventHandler(this.fekete_CheckedChanged);
+            // 
+            // kek
+            // 
+            this.kek.AutoSize = true;
+            this.kek.Location = new System.Drawing.Point(46, 63);
+            this.kek.Name = "kek";
+            this.kek.Size = new System.Drawing.Size(44, 17);
+            this.kek.TabIndex = 11;
+            this.kek.TabStop = true;
+            this.kek.Text = "Kék";
+            this.kek.UseVisualStyleBackColor = true;
+            this.kek.CheckedChanged += new System.EventHandler(this.kek_CheckedChanged);
+            // 
+            // piros
+            // 
+            this.piros.AutoSize = true;
+            this.piros.Location = new System.Drawing.Point(46, 29);
+            this.piros.Name = "piros";
+            this.piros.Size = new System.Drawing.Size(48, 17);
+            this.piros.TabIndex = 10;
+            this.piros.TabStop = true;
+            this.piros.Text = "Piros";
+            this.piros.UseVisualStyleBackColor = true;
+            this.piros.CheckedChanged += new System.EventHandler(this.piros_CheckedChanged);
+            // 
+            // betustilus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cimkelbl);
+            this.Name = "betustilus";
+            this.Text = "Betűstílus";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cimkelbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox athuzott;
+        private System.Windows.Forms.CheckBox alahuzott;
+        private System.Windows.Forms.CheckBox dolt;
+        private System.Windows.Forms.CheckBox felkover;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton betumeret18;
+        private System.Windows.Forms.RadioButton betumeret16;
+        private System.Windows.Forms.RadioButton betumeret14;
+        private System.Windows.Forms.RadioButton betumeret12;
+        private System.Windows.Forms.RadioButton betumeret11;
+        private System.Windows.Forms.RadioButton betumeret10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton fekete;
+        private System.Windows.Forms.RadioButton kek;
+        private System.Windows.Forms.RadioButton piros;
+    }
+}
+
---
+++ weiss/programozas/C#/13.osztaly/betuStilus/betuStilus/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace betuStilus
+{
+    public partial class betustilus : Form
+    {
+        public betustilus()
+        {
+            InitializeComponent();
+        }
+
+        private void piros_CheckedChanged(object sender, EventArgs e)
+        {
+            cimkelbl.ForeColor = Color.Red;
+        }
+
+        private void kek_CheckedChanged(object sender, EventArgs e)
+        {
+            cimkelbl.ForeColor = Color.FromArgb(0,0,255);
+        }
+
+        private void fekete_CheckedChanged(object sender, EventArgs e)
+        {
+            cimkelbl.ForeColor = Color.FromArgb(0, 0, 0);
+        }
+
+        private void betumeret10_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 10;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void betumeret11_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 11;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void betumeret12_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 12;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void betumeret14_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 14;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void betumeret16_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 16;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void betumeret18_CheckedChanged(object sender, EventArgs e)
+        {
+            float betuMeret = 18;
+            cimkelbl.Font = new Font(cimkelbl.Font.Name, betuMeret, cimkelbl.Font.Style);
+        }
+
+        private void felkover_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+            if(!felkover.Checked)
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style ^ FontStyle.Bold);
+            }
+            else
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style | FontStyle.Bold);
+            }
+
+        }
+
+        private void dolt_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+            if (!dolt.Checked)
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style ^ FontStyle.Italic);
+            }
+            else
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style | FontStyle.Italic);
+            }
+        }
+
+        private void alahuzott_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (!alahuzott.Checked)
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style ^ FontStyle.Underline);
+            }
+            else
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style | FontStyle.Underline);
+
+            }
+        }
+
+        private void athuzott_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (!athuzott.Checked)
+            {
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style ^ FontStyle.Strikeout);
+            }
+            else
+            {
+
+                cimkelbl.Font = new Font(cimkelbl.Font, cimkelbl.Font.Style | FontStyle.Strikeout);
+            }
+        }
+    }
+}
