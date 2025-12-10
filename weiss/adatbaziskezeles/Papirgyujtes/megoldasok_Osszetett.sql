a8db9cba9d6ecede1710d73b61edc6de3f844b33
kisgeri <102257694+kisgeri@users.noreply.github.com>
2025-12-10 13:46:14 +0100

    Add files via upload

---
+++ weiss/adatbaziskezeles/Papirgyujtes/megoldasok_Osszetett.sql
@@ -0,0 +1,66 @@
+﻿A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+A. feladat:
+SELECT DISTINCT(nev), osztaly
+	FROM tanulok
+    WHERE nev LIKE "A%" OR nev LIKE "D%" OR nev LIKE "J%"
+    ORDER BY nev
+
+B. feladat:
+SELECT DISTINCT(nev), osztaly
+	FROM tanulok
+    WHERE osztaly LIKE "2A" OR nev LIKE "3B" OR nev LIKE "7A"
+    ORDER BY osztaly
+
+/*másik*/
+SELECT DISTINCT(nev), osztaly
+	FROM tanulok
+    WHERE osztaly IN("2A", "3B", "7A")
+    ORDER BY osztaly
+
+C. feladat:
+SELECT idopont, SUM(mennyiseg) AS "dkg"
+	FROM leadasok
+    WHERE idopont = "2016-10-10" OR idopont = "2016-10-28"
+    GROUP BY idopont
+
+/*másik*/
+SELECT idopont, SUM(mennyiseg) AS "dkg"
+	FROM leadasok
+    WHERE idopont IN("2016-10-10", "2016-10-28")
+    GROUP BY idopont
+
+D. feladat:
+SELECT SUM(mennyiseg) / COUNT(*) AS "Átlag"
+	FROM leadasok
+    WHERE idopont > "2016-10-12" || idopont < "2016-10-07"
+
+/*másik*/
+SELECT SUM(mennyiseg) / COUNT(*) AS "Átlag"
+	FROM leadasok
+    WHERE idopont NOT BETWEEN "2016-10-07" AND "2016-10-12"
+
+E. feladat:
+SELECT DISTINCT(osztaly)
+	FROM tanulok
+    WHERE osztaly NOT LIKE("2A")
+
+/*másik*/
+SELECT DISTINCT(osztaly)
+	FROM tanulok
+    WHERE osztaly NOT IN("2A")
+
+/*megint másik*/
+SELECT DISTINCT(osztaly)
+	FROM tanulok
+    WHERE osztaly != "2A"
+
+F. feladat
+SELECT DISTINCT(osztaly)
+	FROM tanulok
+    WHERE osztaly NOT IN("2A", "3A", "5B")
+
+/*másik*/
+SELECT DISTINCT(osztaly)
+	FROM tanulok
+    WHERE osztaly NOT LIKE("2A") && osztaly NOT LIKE("3A") && osztaly NOT LIKE("5B")
