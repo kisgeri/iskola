f36abc4781f4beac8fd6bfb4f738bc39bafd5cb8
kisgeri <102257694+kisgeri@users.noreply.github.com>
2026-05-24 00:03:45 +0200

    Add files via upload

---
+++ weiss/programozas/C#/13.osztaly/unitTestSajat/gyepteszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gyepteszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gyepteszt")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a989a6a-0e16-4cbd-bf6b-f36f6129a7a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ weiss/programozas/C#/13.osztaly/unitTestSajat/gyepteszt/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GyepKalkulatorNS;
+
+namespace GyepKalkulatorTests
+{
+    [TestClass]
+    public class GyepKalkulatorTests
+    {
+        [TestMethod]
+        public void NormalEset_SikeresSzamitas()
+        {
+            GyepKalkulator kalkulator = new GyepKalkulator();
+            kalkulator.GyepesitestSzamol(10);
+            Assert.AreEqual(39.25, kalkulator.Gyepterulet, 0.01);
+        }
+
+        [TestMethod]
+        public void NullaAtmero_NullaEredmeny()
+        {
+            GyepKalkulator kalkulator = new GyepKalkulator();
+            kalkulator.GyepesitestSzamol(0);
+            Assert.AreEqual(0, kalkulator.Gyepterulet);
+        }
+
+        [TestMethod]
+        public void KicsiAtmero_KisTerulet()
+        {
+            GyepKalkulator kalkulator = new GyepKalkulator();
+            kalkulator.GyepesitestSzamol(1);
+            Assert.IsTrue(kalkulator.Gyepterulet > 0 && kalkulator.Gyepterulet < 1);
+        }
+    }
+}
