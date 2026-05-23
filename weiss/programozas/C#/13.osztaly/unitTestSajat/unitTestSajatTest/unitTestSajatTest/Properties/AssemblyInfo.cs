89800c7ade3ee4309d420e3bb008abb9ceec3294
kisgeri <102257694+kisgeri@users.noreply.github.com>
2026-05-24 00:08:29 +0200

    Add files via upload

---
+++ weiss/programozas/C#/13.osztaly/unitTestSajat/unitTestSajatTest/unitTestSajatTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unitTestSajatTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unitTestSajatTest")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7bf32dc7-865c-436a-8d8d-b6ad6bc2a29d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
