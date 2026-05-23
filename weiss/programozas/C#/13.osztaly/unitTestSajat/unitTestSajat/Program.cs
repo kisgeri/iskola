8c5196a2fce8aeff4ef1b016b1c9df4dfe609be5
kisgeri <102257694+kisgeri@users.noreply.github.com>
2026-05-24 00:07:15 +0200

    Add files via upload

---
+++ weiss/programozas/C#/13.osztaly/unitTestSajat/unitTestSajat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GyepKalkulatorNS
+{
+    public class GyepKalkulator
+    {
+        private double gyepterulet;
+        private double osszar;
+
+        public void GyepesitestSzamol(double atmero)
+        {
+            double r = atmero / 2;
+            gyepterulet = ((r * r) * 3.14) / 2;
+            osszar = gyepterulet * 2500;
+
+            gyepterulet = Math.Round(gyepterulet, 4);
+            osszar = Math.Round(osszar, 2);
+        }
+
+        //Property-k: Olvasható adatok, de nem írhatóak közvetlenül(nem tudom mi ez)
+        public double Gyepterulet
+        {
+            get { return gyepterulet; }
+        }
+
+        public double Osszar
+        {
+            get { return osszar; }
+        }
+
+        public static void Main()
+        {
+            //Adott egy kör alakú telek. A telek felének gyepesítését nekünk kell megoldani.
+            Console.WriteLine("gyepesítés");
+
+            GyepKalkulator kalkulator = new GyepKalkulator();
+
+            Console.Write("Add meg az átmérőt: ");
+            double atmero = Convert.ToDouble(Console.ReadLine());
+
+            
+            kalkulator.GyepesitestSzamol(atmero);
+
+            Console.WriteLine();
+            Console.WriteLine("{0} négyzetméter gyeptégla kell ami {1} Ft-ba kerül",kalkulator.Gyepterulet, kalkulator.Osszar);
+
+            Console.ReadKey();
+        }
+    }
+}
